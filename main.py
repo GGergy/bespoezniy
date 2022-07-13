df8d540d123f35c9de777531bcafdbc7f5c38ee3
Gergy <gergy2k07@gmail.com>
2022-07-13 11:49:57 +0300

    worked queue and playlists

--- main.py
+++ main.py
@@ -7,37 +7,42 @@ from time import sleep
 from discord.ext import commands
 from config import TOKEN
 from threading import Thread
-from timeit import default_timer
 from youtube_dl import YoutubeDL
+from sqlaver import insert_blob, read_blob_data
 
 vc = None
+nxt = False
+pl = {}
 qe = []
-lenght = 0
-zero = 0
 YDL_OPTIONS = {'format': 'worstaudio/best', 'noplaylist': 'False', 'simulate': 'True',
                'preferredquality': '192', 'preferredcodec': 'mp3', 'key': 'FFmpegExtractAudio'}
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 client = commands.Bot(command_prefix='GG ')
 
 
-def wait(ctx, arg, tme=False):
-    global lenght, zero, vc
-    if tme:
-        print(lenght - tme)
-        sleep(lenght - tme)
-    print(12)
-    if arg[-1] == 'txt:true':
-        src = ' '.join(arg[:-1])
-        text(ctx, src)
-    else:
-        src = ' '.join(arg)
-    with YoutubeDL(YDL_OPTIONS) as ydl:
-        info = ydl.extract_info(f"ytsearch:{src}", download=False)['entries'][0]
-    url = info['formats'][0]['url']
-    lenght = info['duration']
-    vc.play(discord.FFmpegPCMAudio(executable="bin\\ffmpeg.exe", source=url, **FFMPEG_OPTIONS))
-    zero = default_timer()
-
+def wait(ctx, lst):
+    global vc, nxt
+    qdl = False
+    if lst == qe:
+        qdl = True
+    while lst:
+        vc.stop()
+        src = ' '.join(lst[0]) if qdl else lst[0]
+        with YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(f"ytsearch:{src}", download=False)['entries'][0]
+        url = info['formats'][0]['url']
+        lenght = info['duration']
+        vc.play(discord.FFmpegPCMAudio(executable="bin\\ffmpeg.exe", source=url, **FFMPEG_OPTIONS))
+        #insert_blob(src, discord.FFmpegPCMAudio(executable="bin\\ffmpeg.exe", source=url, **FFMPEG_OPTIONS).read())
+        del lst[0]
+        for i in range(int(lenght) + 1):
+            if nxt:
+                nxt = False
+                print(1)
+                break
+            sleep(1)
+    if qdl:
+        qe.clear()
 
 
 @client.command()
@@ -105,59 +110,103 @@ async def брось_кубик(ctx, *args):
 
 
 @client.command(pass_context=True)
-async def play(ctx, *arg, end=False, tm=False):
+async def play(ctx, *arg):
     global vc, qe
-    if end:
-        qe.append(arg)
-    else:
+    plst = False
+    if 'list:' in arg[0]:
+        plst = pl[arg[1]]
+        print(plst)
+    elif arg:
         qe.insert(0, arg)
-    print(arg)
     try:
         vc = await ctx.message.author.voice.channel.connect()
     except:
-        server = ctx.message.guild
-        voice_channel = server.voice_client
-        voice_channel.stop()
-    while qe:
-        th1 = Thread(target=wait, args=[ctx, qe[0], tm])
-        th1.start()
-        del qe[0]
+        if not vc:
+            await ctx.send('вы не в голосовом канале')
+            return None
+        await stop(ctx)
+    await ctx.send('включаю...')
+    print(plst.copy())
+    th1 = Thread(target=wait, args=[ctx, plst.copy() if plst else qe])
+    th1.start()
 
 
 @client.command()
 async def stop(ctx):
-    server = ctx.message.guild
-    voice_channel = server.voice_client
-    voice_channel.stop()
+    try:
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+        voice_channel.stop()
+    except:
+        await ctx.send('музыка сейчас не играет, воспользуйтесь командой GG play')
+
 
 
 @client.command()
 async def pause(ctx):
-    server = ctx.message.guild
-    voice_channel = server.voice_client
-    voice_channel.pause()
+    try:
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+        voice_channel.pause()
+    except:
+        await ctx.send('музыка сейчас не играет, воспользуйтесь командой GG play')
+
 
 
 @client.command()
 async def resume(ctx):
-    server = ctx.message.guild
-    voice_channel = server.voice_client
-    voice_channel.resume()
+    try:
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+        voice_channel.resume()
+    except:
+        await ctx.send('музыка сейчас не играет, воспользуйтесь командой GG play')
 
 
 @client.command()
 async def leave(ctx):
-    voice_client = ctx.message.guild.voice_client
-    await voice_client.disconnect()
+    global vc
+    try:
+        voice_client = ctx.message.guild.voice_client
+        await voice_client.disconnect()
+    except:
+        await ctx.send('бот не в голосовом канале')
 
 
 @client.command(pass_context=True)
 async def queue(ctx, *arg):
-    global zero
-    src = ' '.join(arg)
+    global qe
+    if arg in qe:
+        await ctx.send('уже добавлено')
+        return None
+    qe.append(arg)
     await ctx.send('добавлено')
-    print(default_timer() - zero)
-    await play(ctx, src, end=True, tm=default_timer() - zero)
+
+
+@client.command()
+async def next(ctx):
+    global nxt
+    nxt = True
+    if qe:
+        await ctx.send('переключаю..')
+    else:
+        await ctx.send('ваш плейлист кончился')
+        await stop(ctx)
+
+
+@client.command()
+async def commands(ctx):
+    txt = 'GG queue {название песни} - добавляет песню в конец очереди\nGG play {название}/{ничего} добавляет песню в начало очереди и воспроизводит ее\nGG text {название} - выводит текст песни по названию\nGG next - переключает очередь на следующую песню\nGG pause - пауза\nGG resume - снятие с паузы\nGG leave - выход из голосового канала'
+    embed = discord.Embed(title=f"комманды этого бота:", url="https://realdrewdata.medium.com/", description=txt, color=0xA35DE0)
+    await ctx.send(embed=embed)
+
+
+@client.command(pass_context=True)
+async def create_playlist(ctx, *arg):
+    name = arg[0]
+    pl[name] = ' '.join(arg[1:]).split(', ')
+    print(pl)
+    await ctx.send('плейлист создан')
 
 
 
